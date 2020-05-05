8c577c60c4509a9c1599253710082f29bd00d432
Vladimir Ilyin <oivt@ya.ru>
2020-05-05 19:09:39 +0300

    One Activity

--- app/src/main/java/study/android/colornotes/Activity1.kt
+++ app/src/main/java/study/android/colornotes/Activity1.kt
@@ -4,18 +4,32 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity1.*
 
+
 class Activity1 : AppCompatActivity() {
-    
+
+    var colors = arrayOf(0xFFFFFFA0, 0xFFFFA0FF, 0xFFA0FFFF)
+    var sheetNumber = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity1)
+        sheetNumber = getIntent().getIntExtra("sheetNumber", 0)
         next.setOnClickListener() {
-            //Переходим на следующую активность
-            val intent = Intent(this, Activity2::class.java)
-            startActivity(intent)
+            if (sheetNumber < colors.size - 1) { //запускаем свою копию
+                val intent = Intent(this,this::class.java)
+                //закладываем в intent номер следующего листа
+                intent.putExtra("sheetNumber", sheetNumber + 1)
+                startActivity(intent)
+            } else {
+                Toast.makeText(
+                    this, R.string.last_page,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 
@@ -24,20 +38,23 @@ class Activity1 : AppCompatActivity() {
         // открываем редактор изменений
         val prefs = getPreferences(Context.MODE_PRIVATE).edit()
         // записываем данные "ключ"-"значение"
-        prefs.putString("note1", text.editableText.toString())
+        prefs.putString("note" + sheetNumber, text.editableText.toString())
         // подтверждаем сделанные изменения (сохраняем)
         prefs.commit()
     }
 
     override fun onResume() {
         super.onResume()
+        //устанавливаем цвет из массива цветов
+        sheet.setBackgroundColor(colors[sheetNumber].toInt())
         // читаем по ключу
         val savedState =
-            getPreferences(Context.MODE_PRIVATE).getString("note1", null)
+            getPreferences(Context.MODE_PRIVATE).getString("note"+sheetNumber.toString(), null)
         // если есть сохраненная заметка - восстанавливаем
         if (savedState != null) {
             text.setText(savedState)
         }
     }
+
 
 }
